2ee4d8fc859d3b26705cb6c416f71ec0bae7f768
Peter Lejeck <peter.lejeck@gmail.com>
2019-01-07 20:27:19 -0800

    Update database schema

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -1,14 +1,17 @@
 CREATE TABLE files (
   id TEXT PRIMARY KEY,
+  mime_type TEXT NOT NULL,
+  size BIGINT NOT NULL,
   metadata JSONB NOT NULL DEFAULT '{}'
 );
 
-CREATE TABLE "references" (
-  id TEXT PRIMARY KEY,
+CREATE TABLE file_references (
   file_id TEXT NOT NULL,
+  key TEXT NOT NULL,
   service_id TEXT NOT NULL,
-  released_at TIMESTAMP
+  released_at TIMESTAMP,
+  PRIMARY KEY(file_id, key)
 );
 
-CREATE INDEX ON "references" (file_id);
-CREATE INDEX ON "references" (released_at);
+CREATE INDEX ON file_references (file_id);
+CREATE INDEX ON file_references (released_at);
